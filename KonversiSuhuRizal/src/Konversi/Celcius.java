7b0cc212c2168edace541b065f972c3634503313
rizalseptiana12 <riantikinasih06@gmail.com>
2018-11-16 19:41:05 +0700

    v-01

---
+++ KonversiSuhuRizal/src/Konversi/Celcius.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Konversi;
+
+/**
+ *
+ * @author lenovo x220
+ */
+public class Celcius {
+    public double toCalvin(double n){
+        double k;
+        k = n +273.15;
+        return k ;
+    }public double toFahrenheit(double n){
+        double k;
+        k = n *1.8+32;
+        return k ;
+    }public double toCelcius(double n){
+        double k;
+        k = n;
+        return k ;
+    }
+    
+}
